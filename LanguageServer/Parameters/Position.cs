b692f59f080ba5faebcfa784029441535b56c848
WebFreak001 <gh@webfreak.org>
2018-04-03 17:11:37 +0200

    Position & Range utilities

--- LanguageServer/Parameters/Position.cs
+++ LanguageServer/Parameters/Position.cs
@@ -1,8 +1,104 @@
-﻿namespace LanguageServer.Parameters
+﻿using System;
+
+namespace LanguageServer.Parameters
 {
-    public class Position
+    public class Position : IEquatable<Position>
     {
         public long Line { get; set; }
         public long Character { get; set; }
+
+        public Position()
+        {
+            Line = 0;
+            Character = 0;
+        }
+
+        public Position(long line, long character)
+        {
+            Line = line;
+            Character = character;
+        }
+
+        public static bool operator <(Position p1, Position p2)
+        {
+
+            return Comparison(p1, p2) < 0;
+
+        }
+
+        public static bool operator >(Position p1, Position p2)
+        {
+
+            return Comparison(p1, p2) > 0;
+
+        }
+
+        public static bool operator ==(Position p1, Position p2)
+        {
+
+            return Comparison(p1, p2) == 0;
+
+        }
+
+        public static bool operator !=(Position p1, Position p2)
+        {
+
+            return Comparison(p1, p2) != 0;
+
+        }
+
+        public override bool Equals(object obj)
+        {
+
+            if (!(obj is Position)) return false;
+
+            return this.Character == ((Position)obj).Character && this.Line == ((Position)obj).Line;
+
+        }
+
+        public static bool operator <=(Position p1, Position p2)
+        {
+
+            return Comparison(p1, p2) <= 0;
+
+        }
+
+        public static bool operator >=(Position p1, Position p2)
+        {
+
+            return Comparison(p1, p2) >= 0;
+
+        }
+
+        public static int Comparison(Position p1, Position p2)
+        {
+            if (p1.Line < p2.Line)
+                return -1;
+            else if (p1.Line > p2.Line)
+                return 1;
+
+            if (p1.Character < p2.Character)
+                return -1;
+            else if (p1.Character > p2.Character)
+                return 1;
+
+            return 0;
+
+        }
+
+        public bool Equals(Position other)
+        {
+            return other != null &&
+                   Line == other.Line &&
+                   Character == other.Character;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 1927683087;
+            hashCode = hashCode * -1521134295 + Line.GetHashCode();
+            hashCode = hashCode * -1521134295 + Character.GetHashCode();
+            return hashCode;
+        }
     }
 }
--- LanguageServer/Parameters/Range.cs
+++ LanguageServer/Parameters/Range.cs
@@ -4,5 +4,43 @@
     {
         public Position Start { get; set; }
         public Position End { get; set; }
+
+        public Range()
+        {
+            Start = new Position();
+            End = new Position();
+        }
+
+        public Range(Position start, Position end)
+        {
+            Start = start;
+            End = end;
+        }
+
+        public Range(Position pos)
+        {
+            Start = End = pos;
+        }
+
+        public Range(long startLine, long startColumn, long endLine, long endColumn)
+        {
+            Start = new Position(startLine, startColumn);
+            End = new Position(endLine, endColumn);
+        }
+
+        public Range(long line, long column)
+        {
+            Start = End = new Position(line, column);
+        }
+
+        public bool Intersects(Position p)
+        {
+            return p >= Start && p <= End;
+        }
+
+        public bool Intersects(Range other)
+        {
+            return Start <= other.End && other.Start <= End;
+        }
     }
 }
